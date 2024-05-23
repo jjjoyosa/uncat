d8737d754a7787d45e4782177b6d70d572922379
Jenzzo Joyosa <125535579+jjjoyosa@users.noreply.github.com>
2024-05-24 01:48:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function onTouchMove(event) {
     if (!gameWon && selectedBlock.color === 'red' && selectedBlock.x + selectedBlock.width === boardSize) {
         // Refresh the page
         gameWon = true;
-        window.location.href = '/welcome';
+        window.location.href = 'page.html';
     }
 
     renderBoard();
@@ -123,7 +123,7 @@ function onBlockMouseMove(event) {
     if (!gameWon && selectedBlock.color === 'red' && selectedBlock.x + selectedBlock.width === boardSize) {
         // Refresh the page
         gameWon = true;
-        window.location.href = '/welcome';
+        window.location.href = 'page.html';
     }
 
     renderBoard();
